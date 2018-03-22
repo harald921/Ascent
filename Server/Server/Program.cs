2cd31acd2e0752ba999ab6adb798f5acdd1884ad
Harald <harald921@hotmail.com>
2018-03-22 13:46:22 +0100

    Update gitignore

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -4,12 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Server
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+
     }
 }
