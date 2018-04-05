d3e38d77cda0d1cec6ec46f1062027a82893ddc2
Harald <harald921@hotmail.com>
2018-04-05 12:49:04 +0200

    Begin separate User (Does not compile)

--- Server/User.cs
+++ Server/User.cs
@@ -7,71 +7,15 @@ using Lidgren.Network;
 
 public class User
 {
-    public Data data { get; private set; }
-
-    public NetConnection connection { get; private set; }
+    public readonly NetConnection connection;
 
     List<Guid> _creatureIDs = new List<Guid>();
+
 
     public void AddCreature(Guid inCreatureGuid) => _creatureIDs.Add(inCreatureGuid);
     public Guid[] GetCreatures() => _creatureIDs.ToArray();
 
-    public static event Action<User> OnUserLogin;
 
-
-    public User(Data inData) =>
-        data = inData;
-
-
-
-    public static User Login(string inUsername, string inPassword, NetConnection inConnection)
-    {
-        Console.WriteLine(inUsername + " logging in.");
-        
-        // Check if it eixsts, load and return it
-        // DEBUG:
-        User newUser = new User(new Data()
-        {
-            username = inUsername,
-            password = inPassword
-        });
-
-        newUser.connection = inConnection;
-
-        OnUserLogin?.Invoke(newUser);
-
-        Console.WriteLine("NOT PROPERLY IMPLEMENTED YET!");
-        return newUser;
-    }
-
-    public static User Register(string inUsername, string inPassword, NetConnection inConnection)
-    {
-        Console.WriteLine("New User!");
-        Console.WriteLine("Username: " + inUsername);
-        Console.WriteLine("Password: " + inPassword);
-
-        // Check if it already exists, 
-        
-        // else...
-        User newUser = new User(new Data()
-        {
-            username = inUsername,
-            password = inPassword
-        });
-
-        newUser.connection = inConnection;
-
-        OnUserLogin?.Invoke(newUser);
-
-        return newUser;
-
-        // Write to disk
-    }
-
-
-    public struct Data
-    {
-        public string username;
-        public string password;
-    }
+    public User(NetConnection inConnection) =>
+        connection = inConnection;
 }
---
+++ Server/UserManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class UserManager
+{
+    public static event Action<User> OnUserLogin;
+
+    public static User Login(string inUsername, string inPassword, NetConnection inConnection)
+    {
+        Console.WriteLine(inUsername + " logging in.");
+
+        User loggedInUser = Register(inUsername, inPassword, inConnection);
+
+        OnUserLogin?.Invoke(loggedInUser);
+
+        Console.WriteLine("NOT PROPERLY IMPLEMENTED YET!");
+        return loggedInUser;
+    }
+
+    public static User Register(string inUsername, string inPassword, NetConnection inConnection)
+    {
+        Console.WriteLine("New User!");
+        Console.WriteLine("Username: " + inUsername);
+        Console.WriteLine("Password: " + inPassword);
+
+        // Check if it already exists, if so, error 
+
+        // else...
+        User newUser = new User(inConnection);
+
+        OnUserLogin?.Invoke(newUser);
+
+        return newUser;
+
+        // Write to disk
+    }
+}
