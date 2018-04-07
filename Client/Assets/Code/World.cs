143d27f442e9814e91a1a50a868a4b4ccb29c45b
Harald <harald921@hotmail.com>
2018-04-07 21:06:44 +0200

    Add rough outlining in how World class will be structured

---
+++ Client/Assets/Code/World.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    public ChunkManager    chunkManager    { get; private set; }
+    public CreatureManager creatureManager { get; private set; }
+
+
+    void Awake()
+    {
+        chunkManager    = new ChunkManager();
+        creatureManager = new CreatureManager();
+    }
+}
+
+
+// Temp
+
+public class ChunkManager 
+{
+    
+}
+
+public class CreatureManager
+{
+
+}
+
+
+public class Chunk
+{
+    class Data
+    {
+        // The actual data (e.g tile types, etc)
+        // This will be what is used to generate the view
+    }
+
+    class View
+    {
+        // The mesh, model, etc.
+    }
+}
